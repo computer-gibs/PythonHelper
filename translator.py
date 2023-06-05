dc5041f7aa9c110f5312281f8fd2e18075f3fcba
AlexanderK <49559643+Arimaro@users.noreply.github.com>
2023-06-05 15:15:15 +0500

    translator module

--- translator.py
+++ translator.py
@@ -1,9 +1,11 @@
 from transformers import FSMTForConditionalGeneration, FSMTTokenizer
+
 mname = "facebook/wmt19-en-ru"
 tokenizer = FSMTTokenizer.from_pretrained(mname)
 model = FSMTForConditionalGeneration.from_pretrained(mname)
-input = "Machine learning is great, isn't it?"
-input_ids = tokenizer.encode(input, return_tensors="pt")
-outputs = model.generate(input_ids)
-decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(decoded) # Машинное обучение - это здорово, не так ли?
+
+def translate_text(input):
+    input_ids = tokenizer.encode(input, return_tensors="pt")
+    outputs = model.generate(input_ids)
+    decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return decoded
